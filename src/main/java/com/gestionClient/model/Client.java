8cf8a8bed4b55a06312ee6284e50f20a9f98a699
Mustapha Hain <59408051+infohain@users.noreply.github.com>
2025-05-17 20:56:25 +0100

    Update Client.java

--- src/main/java/com/gestionClient/model/Client.java
+++ src/main/java/com/gestionClient/model/Client.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
     private String nom;
     private String ville;
 }
