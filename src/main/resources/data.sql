9fadebf4ce51ae7161a841a514c1619d69fa81ec
Mustapha Hain <59408051+infohain@users.noreply.github.com>
2025-07-26 12:01:56 +0100

    Update data.sql
    update data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,10 @@ INSERT INTO client (id, nom, ville) VALUES
 (2, 'Amal', 'Tanger'),
 (3, 'Reda', 'Rabat'),
 (4, 'David', 'Bordeaux');
+commit;
+INSERT INTO client (id, nom, ville) VALUES
+(5, 'Ali', 'Fes'),
+(6, 'Sara', 'Tanger'),
+(7, 'Amal', 'Rabat'),
+(8, 'Mick', 'Bordeaux');
+commit;
